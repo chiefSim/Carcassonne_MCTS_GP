7440d638d10b78a2e1f107c47757425e54a8be3a
chiefSim <55285230+chiefSim@users.noreply.github.com>
2021-06-24 00:07:21 +0100

    Add files via upload

---
+++ mcts_gp.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 23 13:52:41 2021
+
+@author: Gavin
+"""
+
---
+++ test1.py
@@ -0,0 +1,9 @@
+from Player import RandomPlayer
+from MCTS_Player import MCTSPlayer
+from MCTS_RAVE_Player import MCTS_RAVEPlayer
+from MCTS_GP_Player import MCTS_GP_Player
+from Star1_Player import Star1
+from Star2_5_Player import Star2_5
+
+
+m1 = MCTS_Player(iterations = 10, logs=True)
